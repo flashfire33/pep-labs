0377a33d698126250bfecbbe713f7afe11b698e2
flashfire33 <alexander.h.wahid@gmail.com>
2023-01-11 22:16:53 +0000

    Saved progress on BankAccount 2023-01-11 22:16:53 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-        this.balance = amount;
+        this.balance += amount;
     }
 
     /**
